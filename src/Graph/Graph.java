dadc406524a628fba707809e2e9723d94230ead6
Anmol Agarwal <anmol17agarwal@gmail.com>
2025-07-26 23:51:37 +0530

    created Graph and its methods

---
+++ src/Graph/Graph.java
@@ -0,0 +1,91 @@
+package Graph;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Graph {
+
+    private class Vertex {
+        HashMap<String, Integer> neighbours= new HashMap<>();
+    }
+
+    HashMap<String, Vertex> vertices;
+
+    Graph() {
+        vertices = new HashMap<>();
+    }
+
+    public int getVertexCount() {
+        return vertices.size();
+    }
+
+    public boolean containsVertex(String vName) {
+        return vertices.containsKey(vName);
+    }
+
+    public void addVertex(String vName) {
+        vertices.put(vName, new Vertex());
+    }
+
+    public void removeVertex(String vName) {
+        Vertex vertex = vertices.get(vName);
+        ArrayList<String> keys = new ArrayList<>(vertex.neighbours.keySet());
+        for (String key : keys) {
+            Vertex neighbourVertex = vertices.get(key);
+            neighbourVertex.neighbours.remove(vName);
+
+        }
+        vertices.remove(vName);
+    }
+
+    public int getEdgeCount() {
+        int count = 0;
+        ArrayList<String> edges = new ArrayList<>(vertices.keySet());
+        for (String edge : edges) {
+            Vertex vertex = vertices.get(edge);
+            count += vertex.neighbours.size();
+        }
+        return count / 2;
+    }
+
+    public boolean containsEdge(String vName, String vName2) {
+        Vertex vertex = vertices.get(vName);
+        Vertex vertex2 = vertices.get(vName2);
+        if (vertex == null || vertex2 == null || !vertex.neighbours.containsKey(vName2)) {
+            return false;
+        }
+        return true;
+    }
+
+    public void addEdge(String vName, String vName2, int cost) {
+        Vertex vertex = vertices.get(vName);
+        Vertex vertex2 = vertices.get(vName2);
+        if (vertex == null || vertex2 == null || vertex.neighbours.containsKey(vName2)) {
+            return;
+        }
+        vertex.neighbours.put(vName2, cost);// 2k neighbours put C with given cost
+        vertex2.neighbours.put(vName, cost);// 4k neighbours put A with given cost
+    }
+
+    public void removeEdge(String vName, String vName2) {
+        Vertex vertex = vertices.get(vName);
+        Vertex vertex2 = vertices.get(vName2);
+        if (vertex == null || vertex2 == null || !vertex.neighbours.containsKey(vName2)) {
+            return;
+        }
+        vertex.neighbours.remove(vName2);// 2k neighbours remove C
+        vertex2.neighbours.remove(vName);// 4k neighbours remove A
+    }
+
+    public void printGraph() {
+        System.out.println("======================");
+        ArrayList<String> keys = new ArrayList<>(vertices.keySet());
+        for (String key : keys) {
+            Vertex vertex = vertices.get(key);
+            System.out.println(key+":->"+vertex.neighbours);
+        }
+
+        System.out.println("======================");
+    }
+}
+
---
+++ src/Graph/Main.java
@@ -0,0 +1,46 @@
+package Graph;
+
+public class Main {
+    public static void main(String[] args) {
+        Graph graph = new Graph();
+        graph.addVertex("A");
+        graph.addVertex("B");
+        graph.addVertex("C");
+        graph.addVertex("D");
+        graph.addVertex("E");
+        graph.addVertex("F");
+        graph.addVertex("G");
+
+        graph.addEdge("A", "B", 1);
+        graph.addEdge("A", "D", 2);
+        graph.addEdge("B", "C", 3);
+        graph.addEdge("C", "D", 4);
+        graph.addEdge("D", "E", 5);
+        graph.addEdge("E", "F", 6);
+        graph.addEdge("E", "G", 7);
+        graph.addEdge("F", "G", 8);
+
+        graph.printGraph();
+
+        System.out.println(graph.getVertexCount());
+        System.out.println(graph.getEdgeCount());
+
+        System.out.println(graph.containsEdge("A","B"));
+        System.out.println(graph.containsEdge("A","C"));
+
+        System.out.println(graph.containsVertex("A"));
+
+        graph.removeEdge("A","B");
+        graph.printGraph();
+
+        graph.removeVertex("F");
+        graph.printGraph();
+
+        graph.addVertex("F");
+        graph.printGraph();
+
+        graph.addEdge("F", "E", 1);
+        graph.addEdge("F", "G", 1);
+        graph.printGraph();
+    }
+}
