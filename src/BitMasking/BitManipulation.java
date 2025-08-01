b79f5a9448e14960cf004f8eab3efcd368fb4ed9
Anmol Agarwal <anmol17agarwal@gmail.com>
2025-08-01 17:45:17 +0530

    Bit Manipulation rightmost set bit

--- src/BitMasking/BitManipulation.java
+++ src/BitMasking/BitManipulation.java
@@ -9,6 +9,7 @@ public class BitManipulation {
         }
         System.out.println(setKthBit(22,1));
         System.out.println(resetBit(23,3));
+        System.out.println(rightMostSetBit(6));
     }
     public static int extractKthBit(int n, int k) {
         int mask=1;
@@ -29,5 +30,16 @@ public class BitManipulation {
         mask=~(mask<<(k-1));
         int res=n&mask;
         return res;
+    }
+
+    //it will return the position of the rightmost bit having value 1
+    public static int rightMostSetBit(int n) {
+        int pos=1;
+        int mask=1;
+        while((n&mask)==0){
+            pos++;
+            mask=mask<<1;
+        }
+        return pos;
     }
 }
