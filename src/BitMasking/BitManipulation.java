dde58980fdcba34268317ea4f87a92da4a8284d3
Anmol Agarwal <anmol17agarwal@gmail.com>
2025-08-01 18:14:58 +0530

    Bit Manipulation Questions on bit

--- src/BitMasking/BitManipulation.java
+++ src/BitMasking/BitManipulation.java
@@ -10,6 +10,11 @@ public class BitManipulation {
         System.out.println(setKthBit(22,1));
         System.out.println(resetBit(23,3));
         System.out.println(rightMostSetBit(6));
+        System.out.println(addOne(6));
+        for(int i=0;i<32;i++){
+            System.out.print(magicNumber(i)+" ");
+        }
+
     }
     public static int extractKthBit(int n, int k) {
         int mask=1;
@@ -41,5 +46,49 @@ public class BitManipulation {
             mask=mask<<1;
         }
         return pos;
+    }
+
+    public static int addOne(int n) {
+        int mask=1;
+        while((n&mask)!=0){
+            n=n^mask;
+            mask=mask<<1;
+        }
+        n=n^mask;
+        return n;
+    }
+
+//    Question: Power of 5 or Sum of power of 5
+    public static int magicNumber(int n) {
+        int ans=0;
+        int pow=1;
+        while(n!=0){
+            pow=pow*5;
+            if((n&1)==1){
+                ans=ans+pow;
+            }
+            n=n>>1;
+        }
+        return ans;
+    }
+
+/*
+    Question: there is a pascal triangle of size n
+    1  = 1
+    1 1 = 2
+    1 2 1 = 4
+    1 3 3 1 = 8
+    1 4 6 4 1 = 16
+    1 5 10 10 5 1 = 32
+    now find the sum of all the numbers in the pascal triangle
+*/
+    public static int pascalSum(int n) {
+        int res = (1<<n);
+        return res - 1;
+    }
+
+    // Question: find the number is power of 2 or not
+    public static boolean isPowerOf2(int n) {
+        return (n & (n-1)) == 0;
     }
 }
