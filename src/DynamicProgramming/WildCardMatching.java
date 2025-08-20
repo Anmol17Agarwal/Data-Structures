8865117bbcebdbaf24c217a9af017bd27a715724
Anmol Agarwal <anmol17agarwal@gmail.com>
2025-08-21 00:00:24 +0530

    WildCardMatching recursive TD way

--- src/DynamicProgramming/WildCardMatching.java
+++ src/DynamicProgramming/WildCardMatching.java
@@ -5,6 +5,7 @@ public class WildCardMatching {
     String src = "aaabbabababbbaab";
     String pattern = "aaa*******baab";
         System.out.println(wildCardMatchingRecursive(src,pattern));
+        System.out.println(wildCardMatchingRecursiveTD(src,pattern,0,0));
     }
     public static boolean wildCardMatchingRecursive(String src,String pattern){
         if(src.length()==0 && pattern.length()==0){
@@ -31,6 +32,36 @@ public class WildCardMatching {
         }else if(patternCh == '*'){
             boolean blank = wildCardMatchingRecursive(src,roPattern);
             boolean multiple = wildCardMatchingRecursive(roSrc,pattern);
+            ans = blank || multiple;
+        }else{
+            ans = false;
+        }
+        return ans;
+    }
+
+    public static boolean wildCardMatchingRecursiveTD(String src,String pattern,int svidx,int pvidx){
+        if(src.length()==svidx && pattern.length()==pvidx){
+            return true;
+        }
+        if(src.length()!=svidx && pattern.length()==pvidx){
+            return false;
+        }
+        if(src.length()==svidx && pattern.length()!=pvidx){
+            for (int i = pvidx; i < pattern.length(); i++) {
+                if(pattern.charAt(pvidx)!='*'){
+                    return false;
+                }
+            }
+            return true;
+        }
+        char srcCh=src.charAt(svidx);
+        char patternCh=pattern.charAt(pvidx);
+        boolean ans;
+        if(srcCh == patternCh || patternCh == '?'){
+            ans = wildCardMatchingRecursiveTD(src,pattern,svidx+1,pvidx+1);
+        }else if(patternCh == '*'){
+            boolean blank = wildCardMatchingRecursiveTD(src,pattern,svidx,pvidx+1);
+            boolean multiple = wildCardMatchingRecursiveTD(src,pattern,svidx+1,pvidx);
             ans = blank || multiple;
         }else{
             ans = false;
