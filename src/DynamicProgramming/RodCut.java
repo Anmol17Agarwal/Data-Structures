86e5779157022a8d30d97a0782f2fa95aaab59d8
Anmol Agarwal <anmol17agarwal@gmail.com>
2025-08-20 23:12:27 +0530

    rodCutting recusive way

---
+++ src/DynamicProgramming/RodCut.java
@@ -0,0 +1,28 @@
+package DynamicProgramming;
+
+public class RodCut {
+    public static void main(String[] args) {
+    int[] price = {0,1,5,8,9,10,17,17,20};
+        System.out.println(rodCutRecursion(price, price.length-1));
+    }
+    public static int rodCutRecursion(int[] price,int n){
+        int max = price[n];
+        int left = 1;
+        int right = n-1;
+        while(left<=right){
+
+            int fp = rodCutRecursion(price,left);
+            int sp = rodCutRecursion(price,right);
+            int total = fp+sp;
+
+            if(max<total){
+                max=total;
+            }
+
+            left++;
+            right--;
+        }
+        return max;
+    }
+
+}
