153dc33fcb33f77cff9faf2c326a20c3f3f6cd8f
Anmol Agarwal <anmol17agarwal@gmail.com>
2025-08-20 23:54:00 +0530

    rodCutting BU way

--- src/DynamicProgramming/RodCut.java
+++ src/DynamicProgramming/RodCut.java
@@ -5,6 +5,7 @@ public class RodCut {
     int[] price = {0,1,5,8,9,10,17,17,20};
         System.out.println(rodCutRecursion(price, price.length-1));
         System.out.println(rodCutTD(price, price.length-1,new int[price.length] ));
+        System.out.println(rodCuttingBU(price));
     }
     public static int rodCutRecursion(int[] price,int n){
         int max = price[n];
@@ -47,6 +48,32 @@ public class RodCut {
         }
         strg[n] = max;
         return max;
+    }
+
+    public static int rodCuttingBU(int[] price){
+        int[] strg = new int[price.length];
+        strg[0]=price[0];
+        strg[1]=price[1];
+        for (int n = 2; n < price.length ; n++) {
+            int max = price[n];
+            int left = 1;
+            int right = n-1;
+            while(left<=right){
+
+                int fp = strg[left];
+                int sp = strg[right];
+                int total = fp+sp;
+
+                if(max<total){
+                    max=total;
+                }
+
+                left++;
+                right--;
+            }
+            strg[n] = max;
+        }
+        return strg[strg.length-1];
     }
 
 }
