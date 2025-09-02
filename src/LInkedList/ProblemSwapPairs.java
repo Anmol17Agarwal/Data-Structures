b2923fbcd748339f82d0561fa1676940594452d1
Anmol Agarwal <anmol17agarwal@gmail.com>
2025-09-02 17:57:34 +0530

    leetCodeQuestion

---
+++ src/LInkedList/ProblemSwapPairs.java
@@ -0,0 +1,38 @@
+package LInkedList;
+
+public class ProblemSwapPairs {
+
+    //  Definition for singly-linked list.
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+    }
+
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode firstNode = head;
+        ListNode secondNode = head.next;
+
+        firstNode.next = swapPairs(secondNode.next);
+
+        secondNode.next = firstNode;
+
+        return secondNode;
+    }
+
+}
