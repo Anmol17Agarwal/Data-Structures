d8d1e359e2ee849a87dce6411f7ad88f21ec3ca1
Anmol Agarwal <anmol17agarwal@gmail.com>
2025-09-03 19:21:58 +0530

    RotateList in Linkedlist

---
+++ src/LInkedList/RotateList.java
@@ -0,0 +1,52 @@
+package LInkedList;
+
+public class RotateList {
+
+
+    //  Definition for singly-linked list.
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null || k == 0) {
+            return head;
+        }
+        int length = 1;
+        ListNode tail = head;
+        while (tail.next != null) {
+            tail = tail.next;
+            length++;
+        }
+        tail.next = head;
+        k = k % length;
+        int stepToNewHead = length - k;
+        ListNode newTail = tail;
+        while (stepToNewHead > 0) {
+            newTail = newTail.next;
+            stepToNewHead--;
+        }
+
+        ListNode newHead = newTail.next;
+        newTail.next = null;
+
+        return newHead;
+
+    }
+
+
+}
