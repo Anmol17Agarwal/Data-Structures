328b741e7d130a7dfab33bb84d327f5ebdce6ea8
Anmol Agarwal <anmol17agarwal@gmail.com>
2025-09-06 21:07:58 +0530

    Remove Element

---
+++ src/Extra/RemoveElement.java
@@ -0,0 +1,16 @@
+package Extra;
+
+public class RemoveElement {
+    //leetcode Question no 27
+    public int removeElement(int[] nums, int val) {
+        int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
+
