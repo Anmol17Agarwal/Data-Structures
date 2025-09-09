48d25191705f55868872d479c0390c315c3f0ee2
Anmol Agarwal <anmol17agarwal@gmail.com>
2025-09-09 20:13:38 +0530

    Remove Duplicate Element from Sorted LL 2

--- src/LInkedList/RemoveDuplicateFromSprtedLL2.java
+++ src/LInkedList/RemoveDuplicateFromSprtedLL2.java
@@ -30,11 +30,13 @@ public class RemoveDuplicateFromSprtedLL2 {
         dummy.next = head;
         ListNode prev = dummy;
         ListNode curr = head;
-        while (curr.next != null) {
-            if (curr.val == curr.next.val) {
-                while (curr.val == curr.next.val) {
-                    curr = curr.next;
-                }
+        while (curr != null) {
+            boolean duplicate = false;
+            while (curr.next != null && curr.val == curr.next.val) {
+                curr = curr.next;
+                duplicate = true;
+            }
+            if (duplicate) {
                 prev.next = curr.next;
             } else {
                 prev = prev.next;
@@ -43,5 +45,6 @@ public class RemoveDuplicateFromSprtedLL2 {
         }
         return dummy.next;
     }
-
 }
+
+
