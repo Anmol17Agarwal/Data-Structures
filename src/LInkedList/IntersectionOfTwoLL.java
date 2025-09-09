406b2a9a7b4356ab4f4f128df473510f6aa63659
Anmol Agarwal <anmol17agarwal@gmail.com>
2025-09-09 20:27:04 +0530

    Intersection of two LL

---
+++ src/LInkedList/IntersectionOfTwoLL.java
@@ -0,0 +1,40 @@
+package LInkedList;
+
+
+//LEETCODE QUESTION NO 160
+public class IntersectionOfTwoLL {
+
+    //  Definition for singly-linked list.
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode a = headA;
+        ListNode b = headB;
+
+        while (a != b) {
+            if (a == null) {
+                a = headB;
+            } else {
+                a = a.next;
+            }
+            if (b == null) {
+                b = headA;
+            } else {
+                b = b.next;
+            }
+        }
+        return a;
+    }
+}
+
