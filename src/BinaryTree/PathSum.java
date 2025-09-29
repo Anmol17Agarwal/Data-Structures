548d86f297ad8f4a6cb4f9aa27def4a1c356ce1d
Anmol Agarwal <anmol17agarwal@gmail.com>
2025-09-29 17:11:10 +0530

    leetcode question 112

---
+++ src/BinaryTree/PathSum.java
@@ -0,0 +1,35 @@
+package BinaryTree;
+//Leetcode question number 112
+public class PathSum {
+
+    //Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        if (root == null) {
+            return false;
+        }
+        targetSum -= root.val;
+        if (root.left == null && root.right == null) {
+            if (targetSum == 0) {
+                return true;
+            }
+        }
+        return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
+    }
+
+}
