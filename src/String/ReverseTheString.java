bfd6c5b1aa14f1aaad69c97dbdf23df7a6c2754c
Anmol Agarwal <anmol17agarwal@gmail.com>
2025-12-04 12:00:56 +0530

    reverse word in the sentence

---
+++ src/String/ReverseTheString.java
@@ -0,0 +1,15 @@
+package String;
+
+public class ReverseTheString {
+    public static void main(String[] args) {
+        String input = "   the    sky is blue";
+
+        String[] arr = input.trim().split("\\s+");
+        StringBuilder output = new StringBuilder();
+        for (int i = arr.length-1; i >=0 ; i--) {
+            output.append(arr[i]);
+            if(i>0)output.append(" ");
+        }
+        System.out.println(output);
+    }
+}
