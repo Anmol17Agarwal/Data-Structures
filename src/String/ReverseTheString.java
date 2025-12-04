3c34b80012717504f3bed0df46d49fcb097f2a5c
Anmol Agarwal <anmol17agarwal@gmail.com>
2025-12-04 12:18:21 +0530

    reverse word in the sentence

--- src/String/ReverseTheString.java
+++ src/String/ReverseTheString.java
@@ -1,8 +1,11 @@
 package String;
 
+import java.util.Arrays;
+
 public class ReverseTheString {
     public static void main(String[] args) {
         String input = "   the    sky is blue";
+        String input1="  Hello,,,   world!!  This is---Java   ";
 
         String[] arr = input.trim().split("\\s+");
         StringBuilder output = new StringBuilder();
@@ -11,5 +14,10 @@ public class ReverseTheString {
             if(i>0)output.append(" ");
         }
         System.out.println(output);
+
+
+        input1 = input1.trim().replaceAll("[-!,\\s]+", " ");
+        String[] arr1 = input1.split(" ");
+        System.out.println(Arrays.toString(arr1));
     }
 }
